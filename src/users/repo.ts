77d09226aaab32123f81ce2251c5e6706c702558
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-12-29 17:40:35 +0530

    update user repo

--- src/users/repo.ts
+++ src/users/repo.ts
@@ -21,3 +21,43 @@ export async function createUser(input: CreateUserInput): Promise<User> {
 async function hashPassword(password: string): Promise<string> {
   return await argon2.hash(password);
 }
+
+interface UpdateUserInput {
+  username?: string;
+  email?: string;
+  password?: string;
+  lastLogin?: Date;
+}
+
+export async function updateUser(
+  userId: string,
+  input: UpdateUserInput,
+): Promise<User> {
+  return await db.user.update({
+    data: {
+      username: input.username,
+      email: input.email,
+      passwordHash: input.password
+        ? await hashPassword(input.password)
+        : undefined,
+      lastLoginAt: input.lastLogin,
+    },
+    where: {
+      id: userId,
+    },
+  });
+}
+
+export async function getUserByUsername(
+  username: string,
+): Promise<User | null> {
+  return await db.user.findUnique({ where: { username } });
+}
+
+export async function getUserById(userId: string): Promise<User | null> {
+  return await db.user.findUnique({ where: { id: userId } });
+}
+
+export async function getUserByEmail(email: string): Promise<User | null> {
+  return await db.user.findUnique({ where: { email } });
+}
